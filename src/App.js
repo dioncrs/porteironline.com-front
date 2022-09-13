499ffb2d2f8b9befbb3b8127b383b55803e5ced2
Dion <dioncassiords@gmail.com>
2022-09-13 17:20:38 -0300

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './login/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBKPTp7CVetc7HQugZ6--prLGYmOuwCqaw",
+  authDomain: "porteironline.firebaseapp.com",
+  projectId: "porteironline",
+  storageBucket: "porteironline.appspot.com",
+  messagingSenderId: "1086883921782",
+  appId: "1:1086883921782:web:29c653307c35a8481ac457"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+function App(props) {
+  if (props.isLoggedIn) {
+    return <h1>Bem vindo!</h1>;
+  }
+  else{
+    return <h1>Faça Login</h1>;
+  }
 }
 
 export default App;
