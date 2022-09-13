748fb73f633b9e549e4abd6bd78252ddc7a19428
Dion <dioncassiords@gmail.com>
2022-09-13 17:48:11 -0300

    App ajust

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import './App.css';
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBKPTp7CVetc7HQugZ6--prLGYmOuwCqaw",
-  authDomain: "porteironline.firebaseapp.com",
-  projectId: "porteironline",
-  storageBucket: "porteironline.appspot.com",
-  messagingSenderId: "1086883921782",
-  appId: "1:1086883921782:web:29c653307c35a8481ac457"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 function App(props) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBKPTp7CVetc7HQugZ6--prLGYmOuwCqaw",
+    authDomain: "porteironline.firebaseapp.com",
+    projectId: "porteironline",
+    storageBucket: "porteironline.appspot.com",
+    messagingSenderId: "1086883921782",
+    appId: "1:1086883921782:web:29c653307c35a8481ac457"
+  };
+  
+  // Initialize Firebase
+  initializeApp(firebaseConfig);
+
   if (props.isLoggedIn) {
     return <h1>Bem vindo!</h1>;
   }
@@ -28,3 +23,8 @@ function App(props) {
 }
 
 export default App;
+
+
+
+
+
