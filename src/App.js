aab04afe4dda50453b1624dc63b95d46a0e90aa9
john <johnsdj68@gmail.com>
2023-06-30 15:51:58 -0300

    register

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import './App.css';
+import "./App.css";
 import { initializeApp } from "firebase/app";
-import { LoginPage } from './pages/login/login-page';
-import { MainPage } from './pages/main/main-page';
+//import { LoginPage } from "./pages/login/login-page";
+import { MainPage } from "./pages/main/main-page";
+import { RegisterPage } from "./pages/register/register-page";
 
 function App(props) {
   const firebaseConfig = {
@@ -10,23 +11,17 @@ function App(props) {
     projectId: "porteironline",
     storageBucket: "porteironline.appspot.com",
     messagingSenderId: "1086883921782",
-    appId: "1:1086883921782:web:29c653307c35a8481ac457"
+    appId: "1:1086883921782:web:29c653307c35a8481ac457",
   };
-  
+
   // Initialize Firebase
   initializeApp(firebaseConfig);
 
   if (props.isLoggedIn) {
-    return <MainPage/>;
-  }
-  else{
-    return <LoginPage/>;
+    return <MainPage />;
+  } else {
+    return <RegisterPage/>;
   }
 }
 
 export default App;
-
-
-
-
-
