70964b743a132b7b1a1836df83cdd83264a6ceb0
Asumerai <josuecontasegura@gmail.com>
2023-06-30 16:19:26 -0300

    deixando mais bonito

--- src/pages/login/login-page.js
+++ src/pages/login/login-page.js
@@ -1,35 +1,33 @@
 import React from "react"
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 import {TextField} from "@mui/material";
 import {Button} from "@mui/material";
 export function LoginPage () {
+
     const var1 = 'Teste'
 
     return (   
-      <div className="container">
-        <div className="container-login">
-          <div className="wrap-login">
+      <Card variant="elevation" sx={{ maxWidth: 365,}} className="container">
+        <CardContent>
+          <Box className="wrap-login" sx={{display:"flex",alignItems:"center",justifyContent:"center"}}>
             <form className="login-form">
-
-              <span className="login-form-title"> Login </span>
-                
-              <div className="wrap-input">
+              <span className="login-form-title"> Login </span>                
+              <Box className="wrap-input" sx={{alignContent:"center",gap:"8px" }}>
                 <TextField id="standard-basic" label="Email" variant="standard" />          
-              </div>
-
+              </Box>
               <div className="wrap-input">
                 <TextField id="standard-basic" label="Senha" variant="standard" />                
-              </div>
-
-              <div className="container-login-btn">
-                <Button variant="contained">ENTRAR</Button>
-              </div>
-              <div className="cadastre sua empresa">
-                <spam className="txt1"> cadastre sua empresa </spam>
-                <a className="txt2" href="#">aqui</a>
-              </div>
+              </div>            
             </form>
-          </div>
-        </div>
-      </div>
+          </Box>
+        </CardContent>
+         <CardActions>
+          <Button>Cadastre sua empresa</Button>
+          <Button variant="contained">ENTRAR</Button>
+         </CardActions>
+      </Card>
     );
 }
