bac0b3164b5ec9ce2e15135dc497ec78ed77adea
Asumerai <josuecontasegura@gmail.com>
2023-06-30 17:27:50 -0300

    possivel produto final da interface

--- src/pages/login/login-page.js
+++ src/pages/login/login-page.js
@@ -7,6 +7,8 @@ import TextField from "@mui/material/TextField";
 import {Button, Typography} from "@mui/material";
 import Icon from "@mui/material/Icon";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LockIcon from '@mui/icons-material/Lock';
+import { Loogotipo } from "../../components/Logotipo";
 
 export function LoginPage () {
 
@@ -15,30 +17,49 @@ export function LoginPage () {
     
     
 
-    return (   
-      <Card variant="elevation" sx={{ maxWidth: 365,}} className="container">
+    const card = (   
+      <Card variant="elevation" sx={{ maxWidth: 700,}} className="container">
         <CardContent>
+          <Loogotipo/>
+           <br/>
+           <Typography sx={{display:"flex",alignItems:"center",justifyContent:"center"}}style={{color:"#757575"}} fontWeight="bolt" variant="h6" >Login</Typography>
+           <br/>
         <Box sx={{display: "flex", alignItems: "center", gap: "8px", }}>
           <Icon>
             < AccountCircleIcon sx={{fontSize: 26}}/>
           </Icon>
           <Typography variant="body2">
-            <TextField sx={{ minWidth: 328 }} id="standard-basic" label="Nome da Empresa" variant="standard"/>           
+            <TextField sx={{ minWidth: 328 }} id="standard-basic" label="Email" variant="standard"/>           
           </Typography>
         </Box>
+         <br/>
         <Box sx={{display: "flex", alignItems: "center", gap: "8px", }}>
           <Icon>
-            < AccountCircleIcon sx={{fontSize: 26}}/>
+            < LockIcon sx={{fontSize: 26}}/>
           </Icon>
           <Typography variant="body2">
-            <TextField sx={{ minWidth: 328 }} id="standard-basic" label="Nome da Empresa" variant="standard"/>           
+            <TextField sx={{ minWidth: 328 }} id="standard-basic" label="Senha" variant="standard"/>           
           </Typography>
         </Box>
+         <br/>
         </CardContent>
          <CardActions>
           <Button>Cadastre sua empresa</Button>
-          <Button variant="contained">ENTRAR</Button>
+          <Box sx={{ marginLeft: "auto" }}>
+          <Button href="" variant="contained">
+            Entrar
+          </Button>
+        </Box>
          </CardActions>
       </Card>
+    );
+    return (
+      <Box
+        sx={{display: "flex", justifyContent: "center", alignItems: "center",minHeight: "100vh",width: 400, }}>
+        <Card
+         variant="outlined"sx={{ transformStyle: "preserve-3d",boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.3)",}}>
+          {card}
+        </Card>
+      </Box>
     );
 }
