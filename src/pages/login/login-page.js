f8f8cee5f16f5c75fa0a5123a11133e35b1d872c
Asumerai <josuecontasegura@gmail.com>
2023-06-30 16:54:21 -0300

    novas atualizaçoes

--- src/pages/login/login-page.js
+++ src/pages/login/login-page.js
@@ -3,26 +3,37 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import {TextField} from "@mui/material";
-import {Button} from "@mui/material";
+import TextField from "@mui/material/TextField";
+import {Button, Typography} from "@mui/material";
+import Icon from "@mui/material/Icon";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 export function LoginPage () {
 
+  
     const var1 = 'Teste'
+    
+    
 
     return (   
       <Card variant="elevation" sx={{ maxWidth: 365,}} className="container">
         <CardContent>
-          <Box className="wrap-login" sx={{display:"flex",alignItems:"center",justifyContent:"center"}}>
-            <form className="login-form">
-              <span className="login-form-title"> Login </span>                
-              <Box className="wrap-input" sx={{alignContent:"center",gap:"8px" }}>
-                <TextField id="standard-basic" label="Email" variant="standard" />          
-              </Box>
-              <div className="wrap-input">
-                <TextField id="standard-basic" label="Senha" variant="standard" />                
-              </div>            
-            </form>
-          </Box>
+        <Box sx={{display: "flex", alignItems: "center", gap: "8px", }}>
+          <Icon>
+            < AccountCircleIcon sx={{fontSize: 26}}/>
+          </Icon>
+          <Typography variant="body2">
+            <TextField sx={{ minWidth: 328 }} id="standard-basic" label="Nome da Empresa" variant="standard"/>           
+          </Typography>
+        </Box>
+        <Box sx={{display: "flex", alignItems: "center", gap: "8px", }}>
+          <Icon>
+            < AccountCircleIcon sx={{fontSize: 26}}/>
+          </Icon>
+          <Typography variant="body2">
+            <TextField sx={{ minWidth: 328 }} id="standard-basic" label="Nome da Empresa" variant="standard"/>           
+          </Typography>
+        </Box>
         </CardContent>
          <CardActions>
           <Button>Cadastre sua empresa</Button>
